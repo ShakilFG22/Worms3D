c239e850f51026138bf66b9f1ea0a8b0af3d0c9b
ShakilFG22 <shakil.islam@edu.futuregames.nu>
2022-09-15 16:54:02 +0200

    A reference script
    Turn based script(Only as a reference)

---
+++ Assets/Scripts/BattleSystem.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum BattleState {START, PLAYERTURN, ENEMYTURN, WON, LOST}
+
+public class BattleSystem : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+
+    public Transform playerBattleStation;
+    public Transform enemyBattleStation;
+
+    Unit playerUnit;
+    Unit enemyUnit;
+
+    public Text dialogueText;
+
+    public BattleState state;
+    
+    void Start()
+    {
+        state = BattleState.START;
+        SetupBattle();
+    }
+
+    void SetupBattle()
+    {
+        GameObject playerGO = Instantiate(playerPrefab, playerBattleStation);
+        playerGO.GetComponent<Unit>();
+        
+        GameObject enemyGo = Instantiate(enemyPrefab, enemyBattleStation);
+        enemyUnit = enemyGo.GetComponent<Unit>();
+
+        // dialogueText.text = "A wild " + enemyUnit.unitName + " approaches";
+    }
+}
--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -11,46 +11,9 @@ public class MovementScript : MonoBehaviour
     {
         _characterController = GetComponent<CharacterController>();
     }
-
-    void SimpleMove(Vector3 move)
-    {
-        // Vector3 framerateIndependentMovement = move * Time.deltaTime;
-        // handleMovement(framerateIndependentMovement);
-    }
-    
     void Update()
     {
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         _characterController.Move(move * (Time.deltaTime * speed));
-        // if (charCon)
-        // {
-        //     handleMovement();
-        //     charIsGrounded = charCon.isGrounded;
-        // }
-        
-        
-        // if (Input.GetAxis("Horizontal") != 0)
-        // {
-        //     transform.Translate(-transform.right * (speed * Time.deltaTime * Input.GetAxis("Horizontal")));
-        // }
-        // if (Input.GetAxis("Vertical") != 0)
-        // {
-        //     transform.Translate(-transform.forward * (speed * Time.deltaTime * Input.GetAxis("Vertical")));
-        // }
     }
-
-    // private void OnControllerColliderHit(ControllerColliderHit hit)
-    // {
-    //     Debug.Log("collided with the " + hit.gameObject.name);
-    // }
-
-    // void handleMovement()
-    // {
-    //     float vertical = Input.GetAxis("Vertical");
-    //     float horizontal = Input.GetAxis("Horizontal");
-    //
-    //     currentMovement = new Vector3(-userInput.x, charIsGrounded ? 0.0f : -1.0f, userInput.z) * Time.deltaTime;
-    //     charCon.Move(currentMovement);
-    //
-    // }
 }
